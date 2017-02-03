e71f9de09d44ae1d76b07e32586f8005843f5dcd
MRX151 <402251907@qq.com>
2017-02-03 19:53:24 +0800

    to create

---
+++ src/mainpackage/Run.java
@@ -0,0 +1,15 @@
+package mainpackage;
+
+public class Run
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("check this!");
+	}
+
+}
